494144043e5dd0123bb6a51751b9a251492d7d01
NiaMori <Hellc5261@gmail.com>
2021-01-19 19:48:35 +0800

    feat: use jsDelivr for assets

--- assets/assets.meta.ts
+++ assets/assets.meta.ts
@@ -1,5 +1,7 @@
 const modules = import.meta.globEager('./**/*.{wav,mp3,svg,jpg,png,webp}')
 
+import assetsProvider from './assets-provider.json'
+
 export const assets: {
   [path: string] : {
     src: string
@@ -13,5 +15,16 @@ export const assets: {
 }))
 
 export const url = (path: string) : string => {
+  if (import.meta.env.MODE === 'production') {
+    const index = path as keyof typeof assetsProvider
+    if (assetsProvider[index]) {
+      const { provider } = assetsProvider[index]
+
+      if (provider.length) {
+        return provider[0]
+      }
+    }
+  }
+
   return assets[path].src
 }
---
+++ build/update-assets.js
@@ -0,0 +1,113 @@
+const fs = require('fs')
+const path = require("path")
+const { exec } = require('child_process')
+
+async function* walk (dir) {
+  for await (const d of await fs.promises.opendir(dir)) {
+    const entry = path.join(dir, d.name)
+    if (d.isDirectory()) yield* walk(entry)
+    else if (d.isFile()) yield entry
+  }
+}
+
+const run = async (cmd) => {
+  return new Promise(resolve => {
+    exec(cmd, (error, stdout) => resolve(stdout))
+  })
+}
+
+const getOID = async (file) => {
+  const pointer = await run(`git lfs pointer --file ${file}`)
+  const { oid } = /oid (?<oid>sha256:\w+)/.exec(pointer).groups
+  return oid
+}
+
+const getLastestVersion = async (name) => {
+  return (await run(`npm info ${name} version`)).trim()
+}
+
+const deploy = async (assets) => {
+  const lastVersion = await getLastestVersion('komori-chiyu-button-assets')
+
+  await run('rm -rf .temp')
+  await run('mkdir -p .temp/dist')
+
+  fs.writeFileSync('.temp/README.md', `# komori-chiyu-button-assets
+Assets for website [komori-chiyu-button](https://github.com/NiaMori/komori-chiyu-button)
+`)
+
+  fs.writeFileSync('.temp/package.json', JSON.stringify({
+    name: 'komori-chiyu-button-assets',
+    author: 'niamori',
+    description: 'komori-chiyu-button-assets',
+    main: 'index.js',
+    scripts: {
+      test: 'echo "Error: no test specified" && exit 1'
+    },
+    version: lastVersion
+  }, null, 2))
+
+  const { version } = /v(?<version>\S+)/.exec(await run('bash -c "cd .temp && npm version patch"')).groups
+
+  for (const { file } of assets) {
+    await run(`cp ${file} .temp/dist/`)
+  }
+
+  const info = {}
+  for (const { path: assetsPath, oid } of assets) {
+    const filename = path.basename(assetsPath)
+
+    info[assetsPath] = {
+      src: `https://cdn.jsdelivr.net/npm/komori-chiyu-button-assets@${version}/dist/${filename}`,
+      OID: oid
+    }
+  }
+
+  fs.writeFileSync('.temp/index.js', '' +
+`export default ${JSON.stringify(info, null, 2)}
+`)
+
+  return [version, info]
+}
+
+const main = async () => {
+  const data = JSON.parse(fs.readFileSync('./assets/assets-provider.json').toLocaleString())
+
+  const newAssets = []
+
+  for await (const file of walk('./assets')) {
+    if (!/\.(wav)|(mp3)|(svg)|(jpg)|(png)|(webp)$/.test(file)) {
+      continue
+    }
+
+    const path = file.replace(/^assets\//, '@')
+
+    const oid = await getOID(file)
+
+    if (!data[path] || data[path].OID != oid) {
+      data[path] = {
+        OID: oid,
+        provider: []
+      }
+
+      newAssets.push({
+        file, path, oid
+      })
+    }
+  }
+
+  for (const { path, oid } of newAssets) {
+    console.log(`updating ${path}-${oid}...`)
+  }
+
+  const [version, info] = await deploy(newAssets)
+
+  for (const { path } of newAssets) {
+    data[path].version = version
+    data[path].provider.push(info[path].src)
+  }
+
+  fs.writeFileSync('./assets/assets-provider.json', JSON.stringify(data, null, 2))
+}
+
+main()
