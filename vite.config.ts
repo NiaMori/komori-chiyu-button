343a7b2b72b955ed0b3b491add8902d7db403379
NiaMori <Hellc5261@gmail.com>
2021-01-24 12:24:08 +0800

    refactor: only enable analytics in build mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import { generateMetaInfo } from './build/vite-plugin-generate-meta-info'
+import { analytics } from './build/vite-plugin-analytics'
 
 export default defineConfig({
   plugins: [
     reactRefresh(),
-    generateMetaInfo()
+    generateMetaInfo(),
+    analytics()
   ]
 })
