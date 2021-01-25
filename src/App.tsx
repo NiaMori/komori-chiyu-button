c33794e7d0b8b237875db693843e7fa5e638c873
NiaMori <Hellc5261@gmail.com>
2021-01-25 19:48:29 +0800

    refactor: extract voices-page as seperate component

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import {
   AppBar,
   Avatar,
   Container,
-  Grid,
   IconButton,
   Toolbar,
   Tooltip,
@@ -22,22 +21,7 @@ import {
   TelevisionClassic
 } from 'mdi-material-ui'
 
-import {
-  VoiceInfoPanel,
-  OptionsPanel,
-  Cards,
-  VoicesCard
-} from './components'
-
-import {
-  useVoices,
-  useOptions
-} from './hooks'
-
-import {
-  voices as allVoices,
-  isNewVoice
-} from './data'
+import { VoicesPage } from './pages'
 
 const komoriAA = `
 言いたいことがあるんだよ！
@@ -61,12 +45,6 @@ const App = () : JSX.Element => {
   console.log('Komori......Komori...... 寂しい......')
 
   const theme = useTheme()
-
-  const [options] = useOptions()
-
-  const { voices } = useVoices(options)
-
-  const newVoices = allVoices.filter(isNewVoice)
 
   return (
     <Fragment>
@@ -116,34 +94,7 @@ const App = () : JSX.Element => {
           margin-top: ${theme.spacing(3)}px;
         `}
       >
-        <Grid container spacing = {3} alignItems = 'stretch'>
-          <Grid item md = {6} xs = {12}>
-            <VoiceInfoPanel
-              css = {css`
-                height: 100%;
-              `}
-            />
-          </Grid>
-
-          <Grid item md = {6} xs = {12}>
-            <OptionsPanel
-              css = {css`
-                height: 100%;
-              `}
-            />
-          </Grid>
-        </Grid>
-
-        {
-          newVoices.length
-          && <VoicesCard tag = {'最近更新'} voices = {newVoices}></VoicesCard>
-        }
-
-        <Cards columnGap = {theme.spacing(5)}>
-          {voices.map(([tag, voices]) => (
-            <VoicesCard key = {tag} tag = {tag} voices = {voices}></VoicesCard>
-          ))}
-        </Cards>
+        <VoicesPage />
       </Container>
     </Fragment>
   )
---
+++ src/pages/voices-page.tsx
@@ -0,0 +1,62 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react'
+
+import { Fragment } from 'react'
+
+import { Grid, useTheme } from '@material-ui/core'
+
+import {
+  VoiceInfoPanel,
+  OptionsPanel,
+  Cards,
+  VoicesCard
+} from '../components'
+
+import {
+  useVoices,
+  useOptions
+} from '../hooks'
+
+import {
+  voices as allVoices,
+  isNewVoice
+} from '../data'
+
+export const VoicesPage = () : JSX.Element => {
+  const theme = useTheme()
+
+  const [options] = useOptions()
+  const { voices } = useVoices(options)
+  const newVoices = allVoices.filter(isNewVoice)
+
+  return (<Fragment>
+    <Grid container spacing = {3} alignItems = 'stretch'>
+      <Grid item md = {6} xs = {12}>
+        <VoiceInfoPanel
+          css = {css`
+            height: 100%;
+          `}
+        />
+      </Grid>
+
+      <Grid item md = {6} xs = {12}>
+        <OptionsPanel
+          css = {css`
+            height: 100%;
+          `}
+        />
+      </Grid>
+    </Grid>
+
+    {
+      newVoices.length
+      && <VoicesCard tag = {'最近更新'} voices = {newVoices}></VoicesCard>
+    }
+
+    <Cards columnGap = {theme.spacing(5)}>
+      {voices.map(([tag, voices]) => (
+        <VoicesCard key = {tag} tag = {tag} voices = {voices}></VoicesCard>
+      ))}
+    </Cards>
+  </Fragment>)
+}
