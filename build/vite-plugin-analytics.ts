30d475e241d329bee7e6d5d09e7397c2707b02a8
NiaMori <Hellc5261@gmail.com>
2021-01-26 10:28:05 +0800

    feat: remove cloudflare analytics

--- build/vite-plugin-analytics.ts
+++ build/vite-plugin-analytics.ts
@@ -11,10 +11,6 @@ export const analytics = (): Plugin => {
 
       return html.replace(/<head>/, '' +
 `<head>
-<!-- Cloudflare Web Analytics -->
-<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "5bcd522a79124b53b28304e5a11d47cd"}'></script>
-<!-- End Cloudflare Web Analytics -->
-
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-VLGY6V94TB"></script>
 <script>
