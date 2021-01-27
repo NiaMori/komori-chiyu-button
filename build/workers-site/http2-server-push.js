d4e09804597a3cd10ea131d17160585ca67134ed
NiaMori <Hellc5261@gmail.com>
2021-01-27 08:46:05 +0800

    refactor: adjust cloudflare worker build script

---
+++ build/workers-site/http2-server-push.js
@@ -0,0 +1,78 @@
+import manifest from '../../dist/manifest.json'
+
+const getType = (ext) => {
+  if (ext === 'js') {
+    return 'script'
+  } else if (['svg', 'jpg', 'png'].includes(ext)) {
+    return 'image'
+  }
+
+  throw new Error(`Unkown Extension \`${ext}\``)
+}
+
+const unique = (arr) => {
+  return [...new Set(arr)]
+}
+
+const assets = unique(Object.keys(manifest).map((key) => {
+  const {
+    file,
+    imports = []
+  } = manifest[key]
+
+  return [file, ...imports]
+}).flat()).map((path) => {
+  const [name, hash, ext] = path.split(/\./)
+
+  const type = getType(ext)
+  const crossorigin = type === 'script' ? ' crossorigin=anonymous;' : ''
+
+  return {
+    key: `${name}.${ext}`,
+    hash,
+    push: `</${path}>; rel=preload; as=${type};` + crossorigin
+  }
+})
+
+const getCookie = (request, name) => {
+  let result = ''
+
+  const cookieString = request.headers.get('Cookie')
+
+  if (cookieString) {
+    const cookies = cookieString.split(';')
+    cookies.forEach(cookie => {
+      const cookiePair = cookie.split('=', 2)
+      const cookieName = cookiePair[0].trim()
+      if (cookieName === name) {
+        const cookieVal = cookiePair[1]
+        result = cookieVal
+      }
+    })
+  }
+
+  return result
+}
+
+const setCookie = (response, name, value) => {
+  response.headers.set('Set-Cookie', `${name}=${value}`)
+}
+
+const setUpHttp2ServerPush = (request, response) => {
+  if (response.headers.get('Content-Type').includes('text/html')) {
+    const cookie = getCookie(request, '__http2_server_push')
+
+    const pushed = cookie === '' ? {} : JSON.parse(cookie)
+    const pushing = {}
+
+    for (const { key, hash, push } of assets) {
+      pushing[key] = hash
+
+      if (pushed[key] !== pushing[key]) {
+        response.headers.append('Link', push)
+      }
+    }
+
+    setCookie(response, '__http2_server_push', JSON.stringify(pushing))
+  }
+}
--- build/workers-site/index.js
+++ build/workers-site/index.js
@@ -1,41 +1,5 @@
 import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
 
-import manifest from '../../dist/manifest.json'
-
-const getType = (ext) => {
-  if (ext === 'js') {
-    return 'script'
-  } else if (['svg', 'jpg', 'png'].includes(ext)) {
-    return 'image'
-  }
-
-  throw new Error(`Unkown Extension \`${ext}\``)
-}
-
-const unique = (arr) => {
-  return [...new Set(arr)]
-}
-
-const assets = unique(Object.keys(manifest).map((key) => {
-  const {
-    file,
-    imports = []
-  } = manifest[key]
-
-  return [file, ...imports]
-}).flat()).map((path) => {
-  const [name, hash, ext] = path.split(/\./)
-
-  const type = getType(ext)
-  const crossorigin = type === 'script' ? ' crossorigin=anonymous;' : ''
-
-  return {
-    key: `${name}.${ext}`,
-    hash,
-    push: `</${path}>; rel=preload; as=${type};` + crossorigin
-  }
-})
-
 /**
  * The DEBUG flag will do two things that help during development:
  * 1. we will skip caching on the edge, which makes it easier to
@@ -60,24 +24,7 @@ addEventListener('fetch', event => {
   }
 })
 
-function getCookie (request, name) {
-  let result = ''
-  const cookieString = request.headers.get('Cookie')
-  if (cookieString) {
-    const cookies = cookieString.split(';')
-    cookies.forEach(cookie => {
-      const cookiePair = cookie.split('=', 2)
-      const cookieName = cookiePair[0].trim()
-      if (cookieName === name) {
-        const cookieVal = cookiePair[1]
-        result = cookieVal
-      }
-    })
-  }
-  return result
-}
-
-async function handleEvent(event) {
+const handleEvent = async (event) => {
   try {
     const options = {}
 
@@ -93,23 +40,11 @@ async function handleEvent(event) {
       }
     }
 
+    options.mapRequestToAsset = handlePrefix(/^\/(about)/)
+
     const response = await getAssetFromKV(event, options)
 
-    if (response.headers.get('Content-Type').includes('text/html')) {
-      const cookie = getCookie(event.request, '__http2_server_push')
-      const pushed = cookie === '' ? {} : JSON.parse(cookie)
-      const pushing = {}
-
-      for (const { key, hash, push } of assets) {
-        pushing[key] = hash
-
-        if (pushed[key] !== pushing[key]) {
-          response.headers.append('Link', push)
-        }
-      }
-
-      response.headers.set('Set-Cookie', `__http2_server_push=${JSON.stringify(pushing)}`)
-    }
+    // setUpHttp2ServerPush(event.request, response)
 
     return response
   } catch (e) {
