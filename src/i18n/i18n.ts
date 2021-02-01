d53492caab3149c97b515fb3bfa27a93eb6d38d9
NiaMori <Hellc5261@gmail.com>
2021-02-01 18:48:35 +0800

    feat: improve i18n translation for ja

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import { voices } from '../data'
+import { voices, tagTranslation } from '../data'
 
 export const languages = ['zh-CN', 'ja'] as const
 export type Language = typeof languages[number]
@@ -13,13 +13,32 @@ export const configureI18N = (): void => {
   }> = {
     'ja': {
       translation: {
-        '循环播放': 'ja(循环播放)'
+        '设置': 'ja(设置)',
+        'gachi 浓度': 'ja(gachi 浓度)',
+        '显示所有声音': 'ja(显示所有声音)',
+        '循环播放': 'ja(循环播放)',
+        '允许重叠': 'ja(允许重叠)',
+        '允许涩古守': 'ja(允许涩古守)',
+        '来源': 'ja(来源)',
+        '标签': 'ja(标签)',
+        '关于': 'ja(关于)',
+        '首页': 'ja(首页)',
+        '友情链接': 'ja(友情链接)',
+        '关于「古守血遊按钮站」': 'ja(关于「古守血遊按钮站」)'
       }
     },
 
     'zh-CN': {
       translation: {
         //
+      }
+    }
+  }
+
+  for (const language of languages) {
+    for (const [key, value] of Object.entries(tagTranslation[language] ?? {})) {
+      if (value) {
+        resources[language].translation[key] = value
       }
     }
   }
