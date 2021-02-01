993457b1830a8fde539c5bfb84deb15fb6232dff
NiaMori <Hellc5261@gmail.com>
2021-02-02 00:15:59 +0800

    feat: use language detector

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,5 +1,7 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
 import { voices, tagTranslation } from '../data'
 
 export const languages = ['zh-CN', 'ja'] as const
@@ -57,6 +59,7 @@ export const configureI18N = (): void => {
 
   i18n
     .use(initReactI18next)
+    .use(LanguageDetector)
     .init({
       resources,
       fallbackLng: 'zh-CN',
