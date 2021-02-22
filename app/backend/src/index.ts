d414a467f5b1c14459e218277bcbbd6dd7441f79
NiaMori <Hellc5261@gmail.com>
2021-02-22 16:29:43 +0800

    feat: add simple voice-playback-statistics backend

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -1,12 +1,48 @@
-import { createServer } from 'http'
+import { Server as SocketServer, Socket } from 'socket.io'
 
-import { voices } from '@frontend/src/data'
+import { emit, subscribe } from './remote-event-emitter'
 
-const http = createServer((req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify(voices, null, 2))
-})
+const io = new SocketServer(3002)
 
-http.listen(8080, () => {
-  console.log('listening at localhost:8080...')
+const database: Record<string, number> = {}
+
+io.on('connection', (socket: Socket) => {
+  emit(io, '@voice-playback-statistics/changes', {
+    changes: Object.entries(database).map(([path, count]) => {
+      return {
+        type: 'create',
+        newData: {
+          path,
+          count
+        }
+      }
+    })
+  })
+
+  subscribe(socket, '@voice-playback-statistics/report-playback', ({ path }) => {
+    if (!database[path]) {
+      database[path] = 0
+    }
+
+    const prevCount = database[path]
+    const currCount = database[path] + 1
+
+    database[path] = currCount
+
+    emit(io, '@voice-playback-statistics/changes', {
+      changes: [{
+        type: 'update',
+
+        oldData: {
+          path,
+          count: prevCount
+        },
+
+        newData: {
+          path,
+          count: currCount
+        }
+      }]
+    })
+  })
 })
